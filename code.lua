5715fd7a38aff586226ecbf4b47fe73603cff66d
Varap <99797025+Varap228@users.noreply.github.com>
2023-08-31 11:19:56 +0300

    Update code.lua

--- code.lua
+++ code.lua
@@ -22,8 +22,8 @@ Guii.Parent = game:GetService("CoreGui")
 Guii.Name = "NameGui"
 
 local Framaa = Instance.new("TextButton")
-Framaa.Position = UDim2.new(0, 383, 0, 4)
-Framaa.Size = UDim2.new(0, 100, 0, 100)
+Framaa.Position = UDim2.new(0, 400, 0, 4)
+Framaa.Size = UDim2.new(0, 200, 0, 100)
 Framaa.Parent = Guii
 Framaa.Text = "Left click=Check Murder \n Right click=remove highlight" 
 Framaa.TextScaled = true
@@ -31,7 +31,7 @@ Framaa.TextColor3 = Color3.new(1, 1, 1)
 
 local function OnButtonClick()
     Framaa.BackgroundColor3 = Color3.new(163/255, 162/255, 165/255) 
-    Framaa.Text = "Who"
+    Framaa.Text = "Who impostor ?"
     if game:GetService("ReplicatedStorage").Settings.Impostor.Value ~= "" and game:GetService("ReplicatedStorage").Settings.Impostor.Value ~= game.Players.LocalPlayer.Name then
         if game.Players[game:GetService("ReplicatedStorage").Settings.Impostor.Value].Character:FindFirstChild("Namehighlight") then
             game.Players[game:GetService("ReplicatedStorage").Settings.Impostor.Value].Character.Namehighlight:Destroy()
@@ -40,14 +40,14 @@ local function OnButtonClick()
         highlight.FillColor = game.Players[game:GetService("ReplicatedStorage").Settings.Impostor.Value].PlayerColor.Value
         highlight.OutlineTransparency = 0.1
         highlight.Name = "Namehighlight"
-        Framaa.Text = game:GetService("ReplicatedStorage").Settings.Impostor.Value
+        Framaa.Text = game:GetService("ReplicatedStorage").Settings.Impostor.Value .. "\ntarget: " .. game:GetService("ReplicatedStorage").Settings.Target.Value
         highlight.Parent = game.Players[game:GetService("ReplicatedStorage").Settings.Impostor.Value].Character
         Framaa.BackgroundColor3 = game.Players[game:GetService("ReplicatedStorage").Settings.Impostor.Value].PlayerColor.Value
     end
 end
 
 local function OnButtonRightClick()
-    Framaa.Text = "Who"
+    Framaa.Text = "Who impostor ?"
     Framaa.BackgroundColor3 = Color3.new(163/255, 162/255, 165/255) 
     local players = game:GetService("Players")
     for _, player in pairs(players:GetPlayers()) do
